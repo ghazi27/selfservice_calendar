40b2f49e2ec9670a05b6eb8ec3c147e30da53e0c
Islam Saeed Tosson <etosson@live.com>
2020-07-20 18:27:27 +0300

    Trying the package hijri-converter

--- src/app/modules/calendar/calendar.component.ts
+++ src/app/modules/calendar/calendar.component.ts
@@ -1,32 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { toHijri, toGregorian } from "hijri-converter";
 
 @Component({
-  selector: 'ngx-calendar',
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  selector: "ngx-calendar",
+  templateUrl: "./calendar.component.html",
+  styleUrls: ["./calendar.component.scss"],
 })
 export class CalendarComponent implements OnInit {
   public selectedMoment: Date;
+  public hijri: any;
+  public gregorian: any;
 
   menu = [
     {
-      title: 'NbDatepicker',
-      link: '/calendar1',
-      icon: 'home'
+      title: "NbDatepicker",
+      link: "/calendar1",
+      icon: "home",
     },
     {
-      title: 'OwlDateTime',
-      link: '/calendar2',
-      icon: 'home'
-    }
+      title: "OwlDateTime",
+      link: "/calendar2",
+      icon: "home",
+    },
   ];
 
-  constructor() {
-
-  }
+  constructor() {}
 
   ngOnInit(): void {
-    this.selectedMoment = new Date();
-  }
+    this.hijri = toHijri(2020, 7, 20);
+    console.log("Hijri: " + JSON.stringify(this.hijri));
 
+    this.gregorian = toGregorian(1441, 11, 29);
+    console.log("Gregorian: " + JSON.stringify(this.gregorian));
+  }
 }
